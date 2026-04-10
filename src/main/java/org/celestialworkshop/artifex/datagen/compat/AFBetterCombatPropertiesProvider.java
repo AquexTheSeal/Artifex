57809f5e9908a89c4b7f48a457154bd682dc691d
AquexTheSeal <85479639+AquexTheSeal@users.noreply.github.com>
2026-04-11 02:08:52 +0800

    - adjusted BTC

--- src/main/java/org/celestialworkshop/artifex/datagen/compat/AFBetterCombatPropertiesProvider.java
+++ src/main/java/org/celestialworkshop/artifex/datagen/compat/AFBetterCombatPropertiesProvider.java
@@ -37,7 +37,7 @@ public class AFBetterCombatPropertiesProvider extends BetterCombatPropertiesProv
                     }
                     case FLANGED_MACE -> {
                         this.addAttribute(weapon, new AttributesContainer("bettercombat:mace", new WeaponAttributes(
-                                0, null, null, null, null, new WeaponAttributes.Attack[]{
+                                0, null, null, false, null, new WeaponAttributes.Attack[]{
                                 new WeaponAttributes.Attack(
                                         new WeaponAttributes.Condition[]{},
                                         WeaponAttributes.HitBoxShape.VERTICAL_PLANE,
@@ -57,10 +57,40 @@ public class AFBetterCombatPropertiesProvider extends BetterCombatPropertiesProv
                         this.addAttribute(weapon, new AttributesContainer("bettercombat:claymore", null));
                     }
                     case JAVELIN -> {
-                        this.addAttribute(weapon, new AttributesContainer("bettercombat:lance", null));
+                        this.addAttribute(weapon, new AttributesContainer("bettercombat:lance", new WeaponAttributes(
+                                3.5, null, null, false, "af_javelin", new WeaponAttributes.Attack[]{
+                                        new WeaponAttributes.Attack(
+                                                new WeaponAttributes.Condition[]{},
+                                                WeaponAttributes.HitBoxShape.FORWARD_BOX,
+                                                0.85F, 0, 0.5, "bettercombat:one_handed_stab",
+                                                new WeaponAttributes.Sound("bettercombat:spear_stab"),
+                                                null),
+                                new WeaponAttributes.Attack(
+                                        new WeaponAttributes.Condition[]{},
+                                        WeaponAttributes.HitBoxShape.FORWARD_BOX,
+                                        0.85F, 0, 0.5, "bettercombat:one_handed_stab",
+                                        new WeaponAttributes.Sound("bettercombat:spear_stab"),
+                                        null),
+                                new WeaponAttributes.Attack(
+                                        new WeaponAttributes.Condition[]{},
+                                        WeaponAttributes.HitBoxShape.FORWARD_BOX,
+                                        1.15F, 0, 0.5, "bettercombat:dual_handed_stab",
+                                        new WeaponAttributes.Sound("bettercombat:spear_stab"),
+                                        null)
+                                })
+                        ));
                     }
                     case SPEAR -> {
-                        this.addAttribute(weapon, new AttributesContainer("bettercombat:lance", null));
+                        this.addAttribute(weapon, new AttributesContainer("bettercombat:lance", new WeaponAttributes(
+                                3.5, null, null, false, "af_spear", new WeaponAttributes.Attack[]{
+                                new WeaponAttributes.Attack(
+                                        new WeaponAttributes.Condition[]{},
+                                        WeaponAttributes.HitBoxShape.FORWARD_BOX,
+                                        1.0F, 0, 0.5, "bettercombat:one_handed_stab",
+                                        new WeaponAttributes.Sound("bettercombat:scythe_slash"),
+                                        null)
+                                })
+                        ));
                     }
                     case GLAIVE -> {
                         this.addAttribute(weapon, new AttributesContainer("bettercombat:glaive", null));
@@ -72,10 +102,39 @@ public class AFBetterCombatPropertiesProvider extends BetterCombatPropertiesProv
                         this.addAttribute(weapon, new AttributesContainer("bettercombat:scythe", null));
                     }
                     case KATANA -> {
-                        this.addAttribute(weapon, new AttributesContainer("bettercombat:katana", null));
+                        this.addAttribute(weapon, new AttributesContainer("bettercombat:katana", new WeaponAttributes(
+                                0.0, "bettercombat:pose_two_handed_katana", null, false, "af_katana", new WeaponAttributes.Attack[]{}
+                        )));
                     }
                     case ODACHI -> {
-                        this.addAttribute(weapon, new AttributesContainer("bettercombat:katana", null));
+                        this.addAttribute(weapon, new AttributesContainer("bettercombat:katana", new WeaponAttributes(
+                                3.25, "bettercombat:pose_two_handed_katana", null, true, "af_odachi", new WeaponAttributes.Attack[]{
+                                        new WeaponAttributes.Attack(
+                                                new WeaponAttributes.Condition[]{},
+                                                WeaponAttributes.HitBoxShape.HORIZONTAL_PLANE,
+                                                0.8F, 120, 0.5, "bettercombat:two_handed_slash_horizontal_right",
+                                                new WeaponAttributes.Sound("bettercombat:claymore_swing"),
+                                                null),
+                                        new WeaponAttributes.Attack(
+                                                new WeaponAttributes.Condition[]{},
+                                                WeaponAttributes.HitBoxShape.VERTICAL_PLANE,
+                                                1.2F, 120, 0.5, "bettercombat:two_handed_slash_vertical_left",
+                                                new WeaponAttributes.Sound("bettercombat:claymore_swing"),
+                                                null),
+                                        new WeaponAttributes.Attack(
+                                                new WeaponAttributes.Condition[]{},
+                                                WeaponAttributes.HitBoxShape.VERTICAL_PLANE,
+                                                1.2F, 120, 0.5, "bettercombat:two_handed_slash_vertical_right",
+                                                new WeaponAttributes.Sound("bettercombat:claymore_swing"),
+                                                null),
+                                        new WeaponAttributes.Attack(
+                                                new WeaponAttributes.Condition[]{},
+                                                WeaponAttributes.HitBoxShape.HORIZONTAL_PLANE,
+                                                0.8F, 120, 0.5, "bettercombat:two_handed_slash_horizontal_left",
+                                                new WeaponAttributes.Sound("bettercombat:claymore_swing"),
+                                                null)
+                                })
+                        ));
                     }
                 }
             }
